757519c9def5657c93be99259ad44f39fe525263
cintiacleomara <cintiacleomara@users.noreply.github.com>
2020-07-06 11:31:10 -0300

    exercicio horarios

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar () {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = 15 //data.getHours ()
+msg.innerHTML = `Agora são ${hora} horas. `
+if (hora >= 0 && hora <12) {
+    // Bom Dia!
+    img.src = 'manha.png'
+    document.body.style.background = '#e2cd9f'
+} else if (hora >= 12 && hora <= 18){
+//Boa Tarde!
+    img.src= 'tarde.png'
+    document.body.style.background = '#b9846f'
+} else {
+    //Boa Noite!
+    img.src = 'noite.png'
+    document.body.style.background = '#515154'
+}
+}
